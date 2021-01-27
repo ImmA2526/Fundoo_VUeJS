ea0eebddaa42418dd5f1d6984b68df6f44277158
Imran2526 <imraninfo97@gmail.com>
2021-01-27 21:12:37 +0530

    [Imran] Delete: contents Deleted from Input to Textarea

--- src/Pages/Create.vue
+++ src/Pages/Create.vue
@@ -7,17 +7,19 @@
       <md-card id="card">
         <div class="note">
           <textarea type="text" placeholder="Take Note...." class="input1" />
-        </div>
-
-        <md-card-actions>
-          <div class="icons">
-           
-            <md-icon id="list_Notes">notes</md-icon>
+        
+          <md-icon id="list_Notes">notes</md-icon>
             <md-tooltip id="list_Notes1" md-direction="bottom">List All list_Notes</md-tooltip>
             <md-icon id="brush">brush</md-icon>
             <md-tooltip id="brush1" md-direction="bottom">Note with Drawing</md-tooltip>
             <md-icon id="Notes_Image">image</md-icon>
             <md-tooltip  id="Notes_Image1" md-direction="bottom">Note with Image</md-tooltip>
+          
+        </div>
+
+        <md-card-actions>
+          <div class="icons">
+           
             
           </div>
           <md-button>Action</md-button>
@@ -37,13 +39,13 @@
 
         <md-card-actions>
           <div class="icons">
-            <md-icon id="reminder">notifications</md-icon>
-            <!-- <md-tooltip md-direction="bottom">Reminder</md-tooltip> -->
+            <md-icon id="reminder">notifications_active</md-icon>
+            <md-tooltip md-direction="bottom">Reminde Me</md-tooltip>
           
             <md-icon id="color">color_lens</md-icon>
-            <!-- <md-tooltip md-direction="bottom"></md-tooltip> -->
+            <md-tooltip md-direction="bottom">Select Color</md-tooltip>
             <md-icon id="archive">archive</md-icon>
-            <!-- <md-tooltip md-direction="bottom">Archive</md-tooltip> -->
+            <md-tooltip md-direction="bottom">Archive</md-tooltip>
            
             </div>
           <md-button>Action</md-button>
@@ -101,44 +103,33 @@
   // padding-left: 40px;
 }
 
+
+
+
+
 // Notes 1
 #brush
 {
-    padding-right: 22px
+    padding-right: 22px;
+     padding-bottom: 28px;
 }
 
-
-
-.input1
-{
-  padding-top:20px ;
-  // border:none;
-  height:40px;
-  
-  width: 400px;
-}
-
-.input1, .text
-{
-  padding-top:20px ;
-  // border:none;
-
-  // align-content: center;
-  width: 400px;
-}
 #list_Notes
 {
  margin-right: 20px; 
   color:orange; 
-}
-#Notes_Image
-{
-  
+  padding-bottom: 28px;
 }
 
+#Notes_Image
+{
+  margin-right: 20px; 
+  padding-bottom: 28px;
+}
+// Content NAme
 #brush1
 {
-    padding-right: 180px
+    padding-right: 180px;
 }
 #list_Notes1
 {
@@ -150,6 +141,25 @@
   
 }
 
+// Input Field
+.input1
+{
+  padding-top:10px ;
+  margin-top: 20px;
+  // border:none;
+  height:40px;
+  
+  width: 380px;
+}
+
+ .text
+{
+  padding-top:20px ;
+  // border:none;
+margin-top: 50px;
+  // align-content: center;
+  width: 400px;
+}
 </style>
 
 <script>

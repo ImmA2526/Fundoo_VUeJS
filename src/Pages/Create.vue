7b17cf992d7ad415dc53e897f85ebcf76697f285
Imran2526 <imraninfo97@gmail.com>
2021-01-27 22:06:32 +0530

    [Imran] Delete: Unused Code Remove

--- src/Pages/Create.vue
+++ src/Pages/Create.vue
@@ -1,64 +1,49 @@
 <template>
   <div class="main">
-    <!-- Functionality Pending  -->
-<!-- 1st Note  -->
+    <!-- 1st Note  -->
     <!-- <div v-if="open == false" @click="toggle" class="createnote"> -->
-<div>
+    <div>
       <md-card id="card">
         <div class="note">
           <textarea type="text" placeholder="Take Note...." class="input1" />
-        
-          <md-icon id="list_Notes">notes</md-icon>
-            <md-tooltip id="list_Notes1" md-direction="bottom">List All list_Notes</md-tooltip>
-            <md-icon id="brush">brush</md-icon>
-            <md-tooltip id="brush1" md-direction="bottom">Note with Drawing</md-tooltip>
-            <md-icon id="Notes_Image">image</md-icon>
-            <md-tooltip  id="Notes_Image1" md-direction="bottom">Note with Image</md-tooltip>
           
+          <md-icon id="list_Notes">notes</md-icon>
+          <md-tooltip id="list_Notes1" md-direction="bottom">List All list_Notes</md-tooltip>
+          <md-icon id="brush">brush</md-icon>
+          <md-tooltip id="brush1" md-direction="bottom">Note with Drawing</md-tooltip>
+          <md-icon id="Notes_Image">image</md-icon>
+          <md-tooltip id="Notes_Image1" md-direction="bottom">Note with Image</md-tooltip>
         </div>
-
-        <md-card-actions>
-          <div class="icons">
-           
-            
-          </div>
-          <md-button>Action</md-button>
-          <md-button>Take</md-button>
-        </md-card-actions>
       </md-card>
     </div>
 
-
     <!-- Create Note 2  -->
-<div>
-     <!-- <div v-if="open == false" @click="toggle" class="createnote"> -->
+    <div>
+      <!-- <div v-if="open == false" @click="toggle" class="createnote"> -->
       <md-card id="card1">
         <div class="note">
           <textarea type="text" placeholder="Take Note...." class="text" />
         </div>
 
         <md-card-actions>
-          <div class="icons">
+          <div class="icons1">
             <md-icon id="reminder">notifications_active</md-icon>
-            <md-tooltip md-direction="bottom">Reminde Me</md-tooltip>
-          
+            <md-tooltip id="reminder1" md-direction="bottom">Reminde Me</md-tooltip>
             <md-icon id="color">color_lens</md-icon>
-            <md-tooltip md-direction="bottom">Select Color</md-tooltip>
+            <md-tooltip id="color1" md-direction="bottom">Select Color</md-tooltip>
             <md-icon id="archive">archive</md-icon>
-            <md-tooltip md-direction="bottom">Archive</md-tooltip>
-           
-            </div>
-          <md-button>Action</md-button>
-          <md-button>Take</md-button>
+            <md-tooltip id="archive1" md-direction="bottom">Archive</md-tooltip>
+          </div>
+          <div class="btn">
+            <md-button type="button" class="btn btn-secondary" data-dismiss>Close</md-button>
+          </div>
         </md-card-actions>
       </md-card>
     </div>
-
   </div>
   <!-- main <div></div> -->
 </template>
     
-
 <style lang="scss" scoped>
 // .icons {
 //   display: flex;
@@ -78,87 +63,97 @@
   width: 600px;
 }
 
-// Card 2
-#card1 {
-  height: 180px;
-  width: 600px;
+// Notes 1
+#list_Notes {
+  margin-right: 20px;
+  color: orange;
+  padding-bottom: 28px;
+  margin-left: 20px;
 }
-#edit {
-  color: cornflowerblue;
-  // padding-left: 20px;
+
+#brush {
+  padding-right: 22px;
+  padding-bottom: 28px;
+}
+
+#Notes_Image {
+  margin-right: 20px;
+  padding-bottom: 28px;
+}
+// Content NAme
+#brush1 {
+  padding-right: 180px;
+}
+#list_Notes1 {
+  margin-right: 200px;
+}
+#Notes_Image1 {
+}
+
+// Input Field
+.input1 {
+  padding-top: 10px;
+  margin-top: 20px;
+  border: none;
+  height: 40px;
+
+  width: 380px;
+}
+
+// Create 2 notes
+
+#reminder {
+  color: rgb(12, 12, 12);
+  // padding-left: 30px;
+  margin-left: 40px;
+}
+#color {
+  color: rgb(214, 108, 9);
+  // padding-left: 40px;
+  margin-left: 40px;
 }
 
 #archive {
   color: rgb(19, 2, 2);
   // padding-left: 24px;
+
+  margin-left: 40px;
   // margin-left: 0px;
 }
 
-#reminder {
-  color: rgb(12, 12, 12);
-  // padding-left: 30px;
+.icons1 {
+  margin-right: 280px;
 }
-#color {
-  color: rgb(214, 108, 9);
-  // padding-left: 40px;
+#reminder1 {
 }
-
-
-
-
-
-// Notes 1
-#brush
-{
-    padding-right: 22px;
-     padding-bottom: 28px;
+#color1 {
+}
+#archive1 {
 }
 
-#list_Notes
-{
- margin-right: 20px; 
-  color:orange; 
-  padding-bottom: 28px;
+// Card 2
+#card1 {
+  height: 180px;
+  width: 600px;
 }
 
-#Notes_Image
-{
-  margin-right: 20px; 
-  padding-bottom: 28px;
-}
-// Content NAme
-#brush1
-{
-    padding-right: 180px;
-}
-#list_Notes1
-{
- margin-right: 200px;
-
-}
-#Notes_Image1
-{
-  
+#edit {
+  color: cornflowerblue;
+  // padding-left: 20px;
 }
 
-// Input Field
-.input1
-{
-  padding-top:10px ;
-  margin-top: 20px;
-  // border:none;
-  height:40px;
-  
-  width: 380px;
+.btn {
+  padding-top: 20px;
 }
 
- .text
-{
-  padding-top:20px ;
-  // border:none;
-margin-top: 50px;
+// 2 notes
+.text {
+  padding-top: 20px;
+  border: none;
+  margin-top: 50px;
   // align-content: center;
-  width: 400px;
+  width: 390px;
+  margin-right: 126px;
 }
 </style>
 

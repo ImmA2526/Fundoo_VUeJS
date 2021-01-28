c6312eab7e3e79256f2135acdc811bdcd98f2ff5
Imran2526 <imraninfo97@gmail.com>
2021-01-28 13:25:41 +0530

    [Imran] Add : nav bar Componnent Aded

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,60 @@
 <template>
-<!-- Main -->
-  <div> 
-
-<div class="text" >
-<input type="text" class="textbox" placeholder="Search"/>
-<md-icon id="search">search</md-icon>
-</div>
+  <!-- Main -->
+  <div>
+    <div class="text">
+      <md-icon id="search">search</md-icon><input type="text" class="textbox" placeholder="Search" />
+      
+      <div class="Icons">
+      <md-icon id="refresh">refresh</md-icon>
+      <md-icon id="listview">view_stream</md-icon>
+       <md-icon id="setting">settings</md-icon>
+        <md-icon id ="apps1" class="material-icons">apps</md-icon>
+    </div>
+    </div>
+    
   </div>
   <!-- Main -->
 </template>
+
+<style lang="scss" scoped>
+.textbox
+{
+    width: 520px;
+    border-radius: 12px;
+    height: 40px;
+    margin-top: 20px;
+}
+
+.text
+{
+      margin-left: 270px;
+}
+#search
+{
+    margin-left: 24px;
+}
+#listview
+{
+    margin-left: 24px;
+}
+#setting
+{
+    margin-left: 24px;
+}
+#apps1
+{
+        margin-left: 210px;
+}
+#refresh
+{
+    margin-left: 24px;
+}
+.Icons
+{
+      display: inline;
+    margin-left: 260px;
+// display: contents;
+// margin-left: 100px;
+}
+
+</style>

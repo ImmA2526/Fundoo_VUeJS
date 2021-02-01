fcead2d17aa7a4abb3c23b3e13613b5e5590eb3a
Imran2526 <imraninfo97@gmail.com>
2021-02-01 19:03:48 +0530

    [Imran] Refactor : Home Component Refactored

--- src/Pages/Home.vue
+++ src/Pages/Home.vue
@@ -1,70 +1,82 @@
 <template>
+  <!-- <div class="md-overlay"> -->
   <div class="page-container">
-  <Nav></Nav>
-    <md-app md-mode="reveal">
-      <md-app-toolbar class="md-primary">
-        <md-button class="md-icon-button" @click="menuVisible = !menuVisible">
-          <md-icon>menu</md-icon>
-        </md-button>
-        <span class="md-title">Keep</span>
+    <Nav></Nav>
+    <div class="">
+      <md-app md-mode="reveal">
+        <!-- <div class="md-overlay"></div> -->
+        <md-app-toolbar class="color">
+          <md-button class="md-icon-button" @click="menuVisible = !menuVisible">
+            <md-icon>menu</md-icon>
+          </md-button>
+          <span class="md-title">Keep</span>
 
-<md-icon id="search">search</md-icon>
-      <input type="text" class="textbox" placeholder="Search" />
-      <div class="Icons">
-        <md-icon id="refresh">refresh</md-icon>
-        <md-icon id="listview">view_stream</md-icon>
-        <md-icon id="setting">settings</md-icon>
-        <md-icon id="apps1" class="material-icons">apps</md-icon>
-      </div>
-      </md-app-toolbar>
+          <md-card id="cards">
+            <md-icon id="search">search</md-icon>
+            <input type="text" class="textbox" placeholder="Search" />
+          </md-card>
 
-      <md-app-drawer :md-active.sync="menuVisible">
-        <!-- <md-toolbar class="md-transparent" md-elevation="0">Navigation</md-toolbar> -->
-<div class="md-overlay"></div>
+          <div class="Icons">
+            <md-icon id="refresh">refresh</md-icon>
+            <md-icon id="listview">view_stream</md-icon>
+            <md-icon id="setting">settings</md-icon>
+            <md-icon id="apps1" class="material-icons">apps</md-icon>
+          </div>
+        </md-app-toolbar>
 
-        <md-list>
-          <md-list-item>
-           <md-icon id="notes">notes</md-icon>
-            <span id="notes" class="md-list-item-text">Notes</span>
-          </md-list-item>
+        <md-app-drawer :md-active.sync="menuVisible">
+          <!-- <md-toolbar class="md-transparent" md-elevation="0">Navigation</md-toolbar> -->
+          <!-- </div> -->
 
-          <md-list-item>
-           <md-icon id="reminder">notifications</md-icon>
-            <span id="reminder" class="md-list-item-text">Reminders</span>
-          </md-list-item>
-
-          <md-list-item>
-<md-icon id="archive">archive</md-icon>
-            <span id="archive" class="md-list-item-text">Archive</span>          </md-list-item>
-
-          <md-list-item>
-          <md-icon id="delete">delete</md-icon>
-            <span id="delete" class="md-list-item-text">Trash</span>
+          <md-list>
+            <md-list-item>
+              <md-icon id="notes">notes</md-icon>
+              <span id="notes" class="md-list-item-text">Notes</span>
             </md-list-item>
-        </md-list>
-      </md-app-drawer>
 
-      <md-app-content>
+            <md-list-item>
+              <md-icon id="reminder">notifications</md-icon>
+              <span id="reminder" class="md-list-item-text">Reminders</span>
+            </md-list-item>
 
-      <Create></Create>      
-        <Display></Display>  
-        
-      </md-app-content>
-    </md-app>
+            <md-list-item>
+              <md-icon id="archive">archive</md-icon>
+              <span id="archive" class="md-list-item-text">Archive</span>
+            </md-list-item>
+
+            <md-list-item>
+              <md-icon id="delete">delete</md-icon>
+              <span id="delete" class="md-list-item-text">Trash</span>
+            </md-list-item>
+          </md-list>
+        </md-app-drawer>
+
+        <md-app-content >
+          <Create></Create>
+          <Display></Display>
+        </md-app-content>
+      </md-app>
+    </div>
   </div>
+  <!-- </div> -->
 </template>
 
 <style lang="scss" scoped>
-  .md-app {
+.md-app {
   min-height: 350px;
   // border: 1px solid rgba(#000, .12);
 }
-  .md-drawer {
-  width: 180px;
- 
-  height: 710px;
-    margin-top: 64px;
 
+
+.md-app-content
+{
+  background-color: #fff;
+  
+}
+.md-drawer {
+  width: 180px;
+  height: auto;
+  margin-top: 64px;
 }
 
 .md-content {
@@ -72,36 +84,67 @@
   color: black;
 }
 
-
-  .md-overlay {
-    // position: absolute;
-    // top: 0;
-    // right: 0;
-    // bottom: 0;
-    // left: 0;
-    // z-index: 5;
-    // overflow: hidden;
-    /* background: rgba(0,0,0,.6); */
-    // transition: .35s cubic-bezier(.4,0,.2,1);
-    transition-property: opacity;
-    will-change: opacity;
+.color {
+  background-color: #fff;
+  height: 20px;
 }
 
-// Navabar Style 
+.md-overlay {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 5;
+  overflow: hidden;
+  background: white;
+  // transition: .35s cubic-bezier(.4,0,.2,1);
+  transition-property: opacity;
+  will-change: opacity;
+}
+
+.md-overlay {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 5;
+  overflow: hidden;
+  // background:white;
+  // transition: .35s cubic-bezier(.4,0,.2,1);
+  transition-property: opacity;
+  will-change: opacity;
+}
+
+// Navabar Style
+
+#cards {
+  height: 40px;
+  width: 560px;
+  border-radius: 10px;
+  // border:0.1px solid black;
+  margin-left: 200px;
+}
 
 .textbox {
-  width: 520px;
+  width: 420px;
   border-radius: 12px;
   height: 36px;
-  margin-top: 4px;
+  margin-top: 2px;
+  outline: none;
+  margin-right: 70px;
+  border: none;
 }
 
-.text {
-  margin-left: 40px;
+.Icons {
+  display: inline;
+  margin-left: 240px;
 }
 #search {
-  margin-left: 24px;
+  // margin-left: 24px;
   color: black;
+  margin-right: 12px;
 }
 #listview {
   margin-left: 24px;
@@ -129,36 +172,25 @@
   margin-left: 24px;
   color: black;
 }
-.Icons {
-  display: inline;
-  margin-left: 200px;
-
-  // display: contents;
-  // margin-left: 100px;
-}
-
-
-
 </style>
 
 <script>
 // Importing One Component To Another
 // import Create from "../components/Create";
-import Create from "../components/Create"
-import Nav from "../components/NavBar"
-import Display from '../components/Display.vue'
+import Create from "../components/Create";
+// import Nav from "../components/NavBar";
+import Display from "../components/Display.vue";
 // import icons from "../components/icons"
 export default {
   components: {
     Create,
-    Nav,
+    // Nav,
     Display,
     // icons,
   },
-  name: 'Reveal',
+  name: "Reveal",
   data: () => ({
-    menuVisible: false
+    menuVisible: false,
   }),
-
 };
 </script>

81d9f869ee2b7d8310fab7ce2c0519deb5b55a7d
Imran2526 <imraninfo97@gmail.com>
2021-02-02 10:42:09 +0530

    [Imran] Refactor : Display Component Refactored

--- src/components/Display.vue
+++ src/components/Display.vue
@@ -1,35 +1,28 @@
 <template>
   <div>
     <!-- <md-card> -->
-      <!-- <md-card-content> -->
-          <div class="display" v-for="note in notes " :key="note._id">
-<div class="addNote">
-<div class="notes1">
-            <div class="note pds">
-              <b>{{ note.title }}</b>
-            </div>
-            <div class="note pds">
-              {{ note.description }}
-            </div>
-            <div class="icon">
-             <md-icon id="delete">delete</md-icon>
-              <md-icon id="edit">edit</md-icon>
-            </div>
-<!-- <md-icon>trash</md-icon> -->
-</div>
+    <!-- <md-card-content> -->
+    <div class="display" v-for="note in notes" :key="note._id">
+      <div class="addNote">
+        <div class="notes1">
+          <div class="note pds">
+            <b>{{ note.title }}</b>
+          </div>
+          <div class="note pds">
+            {{ note.description }}
+          </div>
+          <div class="icon">
+            <md-icon id="delete">delete</md-icon>
+            <md-icon id="edit">edit</md-icon>
           </div>
           </div>
-
-          
-          <!-- this is the card body -->
-    
-      <!-- </md-card-content> -->
-    <!-- </md-card> -->
-    <!-- </md-ripple> -->
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+
 .md-card {
   width: 260px;
   margin: 4px;
@@ -39,171 +32,99 @@
   border-radius: 12px;
 }
 
-
-
 .display {
-    display: flex;
-    width: 240px;
-    overflow-wrap: break-word;
-    content: "";
-    display: table;
-    clear: both;
-       margin: -16px -4px;
-    // margin: 0 -5px;
-    display: inline-flex;
-    white-space: normal;
-    margin-left: 1%;
-    margin-top: 3%;
-    
-    /* margin-right: 5%; */
-    
+  display: flex;
+  width: 240px;
+  overflow-wrap: break-word;
+  content: "";
+  display: table;
+  clear: both;
+  margin: -16px -4px;
+  display: inline-flex;
+  white-space: normal;
+  margin-left: 1%;
+  margin-top: 3%;
 }
 
-#edit{
+#edit {
   cursor: pointer;
   margin-left: 20px;
 }
-#delete{
-cursor: pointer;
-height: 2px;
-margin-left: 140px;
+
+#delete {
+  cursor: pointer;
+  height: 2px;
+  margin-left: 140px;
 }
 
-.display-note{
-   
-    margin-bottom: 100%;
-    /* width: 25%; */
+.display-note {
+  margin-bottom: 100%;
   padding: 0 10px;
   display: inline-flex;
   width: 220px;
   margin-left: 10%;
   margin-right: 20%;
 }
+
 .addNote {
-    width: 240px;
-    height:max-content;
-    margin: 5px;
-    border: 1px solid #dadce0;
-    border-radius: 8px;
-        box-sizing: content-box;
+  width: 240px;
+  height: max-content;
+  margin: 5px;
+  border: 1px solid #dadce0;
+  border-radius: 8px;
+  box-sizing: content-box;
 }
 
 .notes1 {
-    height:fit-content;
-    overflow:content;
+  height: fit-content;
+  overflow: content;
 }
 
 .pds {
-    letter-spacing: .00625em;
-    padding: 5px;
-    height:fit-content;
-    /* padding: 16px; */
+  letter-spacing: 0.00625em;
+  padding: 5px;
+  height: fit-content;
 }
-
-
-// @media screen and (max-width: 600px) {
-//     .display {
-//         width:90%;
-//     }
-
-//     .addNote {
-//         width: 90%;
-//     }
-// }
 
 </style>
 
 <script>
+
 import noteService from "../Services/noteService";
 export default {
   components: {
     // Display,
     // icons,
-
   },
 
   data: () => ({
-    notes:[],
+    notes: [],
   }),
   methods: {
     close() {
       this.open = false;
     },
 
-///Get all notes
-  x: function () {
-    noteService
-      .getNotes()
-      .then((response) => {
-        console.log(response.data.data.data);
-        this.notes = response.data.data.data;
-        // this.notes.map(function(title,description){
-
-        // })
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  },
-
-
+    ///Get all notes
+    x: function () {
+      noteService
+        .getNotes()
+        .then((response) => {
+          console.log(response.data.data.data);
+          this.notes = response.data.data.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
   },
 
   //Get Note Function
 
-  
-created() {
+  created() {
     this.x();
   },
 
-
-  mounted() {
-    this.x();
-  },
-
-  //  display() {
-  //       const userData = {
-  //         title: this.title,
-  //         description: this.description,
-  //       };
-  //       noteService
-  //         .getNotes(userData)
-  //         .then(function (data) {
-
-  //           localStorage.getItem("AccessToken");
-
-  //           // Headers .........................
-  //  headers: {
-  //             Authorization: localStorage.getItem("AccessToken");
-  //           }
-  //  setTimeout(()=>  this.$router.push("/home"), 2000)
-  // this.$router.push("/home");
-
-  //       console.log(data);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // },
-
-  // get: function () {
-  //   this.$http
-  //     .get(
-  //       "http://fundoonotes.incubation.bridgelabz.com/api/notes/getNotesList",
-  //       {
-  //         title: this.title,
-  //         description: this.description,
-  //         cartId: "",
-  //       }
-  //     )
-  //     .then(function (data) {
-  //       this.$router.push("/display");
-  //       console.log(data);
-  //     })
-  //     .catch();
-  // },
-
-  // 
 };
 </script>
 
